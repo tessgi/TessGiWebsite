065822e5fbe0615bf98124b83e97459ddb1bdc50
Knicole Colon <knicole.colon@gmail.com>
2019-06-18 16:20:27 -0400

    update meetings on front page

--- pelicanconf.py
+++ pelicanconf.py
@@ -230,9 +230,15 @@ MEETINGS = (
             # ('<b>15 April - 28 June 2019</b><br>'
             #  'Better Stars, Better Planets: Exploiting the Stellar-Exoplanetary Synergy',
             #  'https://www.kitp.ucsb.edu/activities/exostar19'),
+            ('<b>22-26 July 2019</b><br>'
+             'TASC5/KASC12 Workshop',
+             'http://web.mit.edu/tasc5/index.html'),    
             ('<b>29 July - 2 August 2019</b><br>'
              'TESS Science Conference',
-             'https://tess.mit.edu/news/tess-science-conference/'),            
+             'https://tess.mit.edu/news/tess-science-conference/'),
+            ('<b>4-8 November 2019</b><br>'
+             'SEEC Symposium: Rocky Exoplanets in the Era of JWST: Theory and Observation',
+             'https://seec.gsfc.nasa.gov/Events/Symposia/RockyExoplanetsJWSTEra_2019/index.html'),
             )
 
 # Defines the "related websites" listing in the footer of all pages
