1875b3c7d14d5ae569b1ff09558cbf6b8aae849f
rebekah9969 <rebekahhounsell@gmail.com>
2022-08-26 10:50:18 -0400

    made a blank file

--- themes/pelican-bootstrap3-kepler/static/js/drn.js
+++ themes/pelican-bootstrap3-kepler/static/js/drn.js
@@ -1,6 +1,1 @@
-$("#sectors").change(function(){
-var optionSelected = $("option:selected", this);
-    var valueSelected = this.value;
-    $(".drn").hide();
-    $("#" + valueSelected).show();
-})
+
