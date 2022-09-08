ba212c7f7fa1cdc0df8c72d95e451cd3e6456622
rebekah9969 <rebekahhounsell@gmail.com>
2022-09-08 19:28:12 -0400

    Generate live site

--- theme/js/drn.js
+++
@@ -1,6 +0,0 @@
-$("#sectors").change(function(){
-var optionSelected = $("option:selected", this);
-    var valueSelected = this.value;
-    $(".drn").hide();
-    $("#" + valueSelected).show();
-})
