5644a27987c1b580616d01198ca2ae808704d04f
rebekah9969 <rebekahhounsell@gmail.com>
2023-02-02 14:35:41 -0500

    added in new tesspoint tool link in the key info area

--- pelicanconf.py
+++ pelicanconf.py
@@ -136,7 +136,7 @@ KEY_INFORMATION = (
     ('The TESS extended mission', 'extended.html'),
     ('Telescope Information', 'the-tess-space-telescope.html'),
     ('How to access the data', 'data-access.html'),
-    ('Web TESS Target Tool', 'https://heasarc.gsfc.nasa.gov/cgi-bin/tess/webtess/wtv.py'),
+    ('NEW! TESS-point Web Tool', 'https://heasarc.gsfc.nasa.gov/wsgi-scripts/TESS/TESS-point_Web_Tool/TESS-point_Web_Tool/wtv_v2.0.py'),
     ('Observing dates', 'sector.html'),
     ('Proposing science', 'proposing-investigations.html'),
     ('Citizen Science projects', 'citizenscience.html'),
