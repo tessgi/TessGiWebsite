2806573576e434418388097471cda4cbcafa9593
Christina Hedges <christina.l.hedges@nasa.gov>
2023-08-09 14:30:13 -0400

    added TUC page

---
+++ content/pages/the-mission/TUC.md
@@ -0,0 +1,25 @@
+Title: TESS Users Committee
+Save_as: TUC.html
+Date: 2023-8-09 14:00
+Modified: 2023-8-09 14:30
+Author: Allison Youngblood
+ 
+The TESS Users Committee (TUC) is a group of approximately ten members from relevant science communities and external to the TESS project. The goal of the TUC is to provide input to the project on behalf of the user community in order to boost TESS’s impact within the scientific community over the mission’s lifetime. The TUC shall also assist the TESS Project Scientist in preparation for Senior Reviews of the mission during its operational phase. 
+
+Key areas that the TUC is anticipated to examine include: data processing pipelines, data products, data access, data analysis tools, documentation, General Investigator (GI) program policies and implementation, and observing strategies to optimize science return. 
+
+The [TUC Charter](data/TESS_Users_Committee_Charter_2023Mar29.pdf) describes in more detail the goals and structure of the TUC. Please contact the TUC chair or any member for more information or to submit an idea or topic for a future TUC meeting. 
+ 
+### 2023-2025 TUC Chair and Members 
+- Dan Huber (chair, email: <dhuber@hawaii.edu>), University of Hawai’i & University of Sydney  
+- Marcel Agüeros, Columbia University 
+- Luke Bouma, Caltech 
+- Nora Eisner, Flatiron Institute, Center for Computational Astrophysics & Princeton University 
+- Adina Feinstein, University of Colorado Boulder 
+- Teruyuki Hirano, Astrobiology Center / National Astronomical Observatory of Japan  
+- Savita Mathur, Instituto de Astrofisica de Canarias 
+- Armin Rest, STScI/JHU 
+- Malena Rice, Yale University 
+- Krista Lynne Smith, Texas A&M University 
+  
+*Modified August 9th 2023*
--- pelicanconf.py
+++ pelicanconf.py
@@ -6,32 +6,32 @@ import datetime
 import pelican
 
 # If `DEVMODE = True`, show a red warning banner at the top
-DEVMODE = False   # pelicanconf-dev.py will override this
+DEVMODE = False  # pelicanconf-dev.py will override this
 
 # By default, use agressive caching.
 # The Makefile ensures we use `--ignore-cache` for production builds.
 CACHE_CONTENT = True
 LOAD_CONTENT_CACHE = True
-CHECK_MODIFIED_METHOD = 'mtime'
-CONTENT_CACHING_LAYER = 'generator'  # This causes an empty news page
+CHECK_MODIFIED_METHOD = "mtime"
+CONTENT_CACHING_LAYER = "generator"  # This causes an empty news page
 WITH_FUTURE_DATES = False
 
-ANALYTICS = ()   # pelicanconf-live.py will override this
+ANALYTICS = ()  # pelicanconf-live.py will override this
 
-AUTHOR = u'Thomas Barclay'
+AUTHOR = "Thomas Barclay"
 SITENAME = "TESS"
 BANNER_SUBTITLE = "Science Support Center"
 SITEURL = "https://heasarc.gsfc.nasa.gov/docs/tess"
 FULLURL = "https://heasarc.gsfc.nasa.gov/docs/tess"
-SITELOGO = 'images/NASA_logo_vector_lg.png'
+SITELOGO = "images/NASA_logo_vector_lg.png"
 SITELOGO_SIZE = 32
-FAVICON = 'images/favicon.png'
-TIMEZONE = 'America/New_York'
-DEFAULT_LANG = u'en'
+FAVICON = "images/favicon.png"
+TIMEZONE = "America/New_York"
+DEFAULT_LANG = "en"
 
-PATH = 'content'
+PATH = "content"
 THEME = "themes/pelican-bootstrap3-kepler"
-BOOTSTRAP_THEME = 'cosmo'
+BOOTSTRAP_THEME = "cosmo"
 BOOTSTRAP_FLUID = False
 
 BANNER = "images/tess-banner-inverted_v2.jpg"
@@ -59,130 +59,149 @@ TWITTER_USERNAME = "tesshelp"
 
 HIDE_SIDEBAR = True
 
-if pelican.__version__ >= '3.7.0':
+if pelican.__version__ >= "3.7.0":
     MARKDOWN = {
-        'extension_configs': {
-            'markdown.extensions.toc': {},
-            'markdown.extensions.tables': {},
+        "extension_configs": {
+            "markdown.extensions.toc": {},
+            "markdown.extensions.tables": {},
         },
-        'extensions': ['mdx_include']
+        "extensions": ["mdx_include"],
     }
 else:
-    MD_EXTENSIONS = (['toc'])
+    MD_EXTENSIONS = ["toc"]
 
 
 # Which static data dirs should be uploaded as part of the website?
-STATIC_PATHS = (['images', 'data', 'docs'])
+STATIC_PATHS = ["images", "data", "docs"]
 
 # Directories that contain html files we want to exclude
 # because they are sub-pages included through rst includes
-PAGE_EXCLUDES = ['pages/observing/approved-programs', ]
+PAGE_EXCLUDES = [
+    "pages/observing/approved-programs",
+]
 
 # The fancy table of contents sidebar requires a plugin
-PLUGIN_PATHS = [os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                             "plugins")]
-PLUGINS = ['extract_toc']
+PLUGIN_PATHS = [os.path.join(os.path.dirname(os.path.realpath(__file__)), "plugins")]
+PLUGINS = ["extract_toc"]
 
 # Defines the menu items in the top bar
 MENUITEMS = (
-        ('Mission', (
-            ('Objectives', 'objectives.html'),
-            ('Telescope', 'the-tess-space-telescope.html'),
-            ('Primary mission', 'primary.html'),
-            ('First Extended mission', 'extended.html'),
-            ('Second Extended mission', 'second-extended.html'),
-            ('FAQ', 'faq.html'),
-            ('Citizen Science', 'citizenscience.html'),
-            ('Outreach resources', 'gallery.html'),
-            ('Publications', 'publications.html'),
-            ('Media Request', 'media.html'),
-                    )
+    (
+        "Mission",
+        (
+            ("Objectives", "objectives.html"),
+            ("Telescope", "the-tess-space-telescope.html"),
+            ("Primary mission", "primary.html"),
+            ("First Extended mission", "extended.html"),
+            ("Second Extended mission", "second-extended.html"),
+            ("Users Committee", "TUC.html"),
+            ("FAQ", "faq.html"),
+            ("Citizen Science", "citizenscience.html"),
+            ("Outreach resources", "gallery.html"),
+            ("Publications", "publications.html"),
+            ("Media Request", "media.html"),
         ),
-        ('Data & tools', (
-            ('Data pipeline', 'data-handling.html'),
-            ('Data products', 'data-products.html'),
-            ('Data access', 'data-access.html'),
-            ('Data analysis tools', 'data-analysis-tools.html'),
-            ('Community products', 'community.html'),
-            ('Community tools', 'community-tools.html'),
-            ('Documentation', 'documentation.html'),
-            ('Data release notes', 'data_release_notes.html'),
-                      )
+    ),
+    (
+        "Data & tools",
+        (
+            ("Data pipeline", "data-handling.html"),
+            ("Data products", "data-products.html"),
+            ("Data access", "data-access.html"),
+            ("Data analysis tools", "data-analysis-tools.html"),
+            ("Community products", "community.html"),
+            ("Community tools", "community-tools.html"),
+            ("Documentation", "documentation.html"),
+            ("Data release notes", "data_release_notes.html"),
         ),
-        ('Observations', (
-          #  ('TESS at a glance', 'quicklook-mission.html'),
-            ('Technical details', 'observing-technical.html'),
-            ('Sector dates', 'sector.html'),
-            ('Approved GI programs', 'approved-programs.html'),
-                         )
+    ),
+    (
+        "Observations",
+        (
+            #  ('TESS at a glance', 'quicklook-mission.html'),
+            ("Technical details", "observing-technical.html"),
+            ("Sector dates", "sector.html"),
+            ("Approved GI programs", "approved-programs.html"),
         ),
-        ('Proposing', (
-            ('Guest Investigator Proposals', 'proposing-investigations.html'),
-            ('Proposal & observation tools', 'proposal-tools.html'),
-            ("Director's Discretionary Target program", 'ddt.html'),
-            ('Proposal templates', 'proposal-templates.html'),
-                      )
+    ),
+    (
+        "Proposing",
+        (
+            ("Guest Investigator Proposals", "proposing-investigations.html"),
+            ("Proposal & observation tools", "proposal-tools.html"),
+            ("Director's Discretionary Target program", "ddt.html"),
+            ("Proposal templates", "proposal-templates.html"),
         ),
-        # ('Education & Outreach', (
-        #     ('Resources', 'gallery.html'),
-        #     ('Media support', 'media.html'),
-        #                          )
-        # ),
-        #
-        )
+    ),
+    # ('Education & Outreach', (
+    #     ('Resources', 'gallery.html'),
+    #     ('Media support', 'media.html'),
+    #                          )
+    # ),
+    #
+)
 
 # Defines the "key information" box on the front page
 KEY_INFORMATION = (
-    ('The TESS extended mission', 'extended.html'),
-    ('Telescope Information', 'the-tess-space-telescope.html'),
-    ('How to access the data', 'data-access.html'),
-    ('NEW! TESS-point Web Tool', 'https://heasarc.gsfc.nasa.gov/wsgi-scripts/TESS/TESS-point_Web_Tool/TESS-point_Web_Tool/wtv_v2.0.py'),
-    ('Tutorials','https://heasarc.gsfc.nasa.gov/docs/tess/data-analysis-tools.html'),
-    ('Observing dates', 'sector.html'),
-    ('Proposing science', 'proposing-investigations.html'),
-    ('Citizen Science projects', 'citizenscience.html'),
-    ('Volunteer to serve on a review','https://goo.gl/forms/p4ZqiTQSEHjbM6nz2'),
-    ('Do you have a news-worthy TESS result?', 'media.html'),
-    ('Outreach Resources', 'gallery.html'),
-    ('Publications', 'publications.html'),
-    )
+    ("The TESS extended mission", "extended.html"),
+    ("Telescope Information", "the-tess-space-telescope.html"),
+    ("How to access the data", "data-access.html"),
+    (
+        "NEW! TESS-point Web Tool",
+        "https://heasarc.gsfc.nasa.gov/wsgi-scripts/TESS/TESS-point_Web_Tool/TESS-point_Web_Tool/wtv_v2.0.py",
+    ),
+    ("Tutorials", "https://heasarc.gsfc.nasa.gov/docs/tess/data-analysis-tools.html"),
+    ("Observing dates", "sector.html"),
+    ("Proposing science", "proposing-investigations.html"),
+    ("Citizen Science projects", "citizenscience.html"),
+    ("Volunteer to serve on a review", "https://goo.gl/forms/p4ZqiTQSEHjbM6nz2"),
+    ("Do you have a news-worthy TESS result?", "media.html"),
+    ("Outreach Resources", "gallery.html"),
+    ("Publications", "publications.html"),
+)
 
 
 # Defines the "important dates" box on the front page
 IMPORTANT_DATES = (
-    ('<b>7th January 2023</b>',
-             'AAS #241 Workshop: NASAs TESS Mission Interactive Data Workshop',
-             '',),
-    ('<b>10th January 2023</b>',
-             'AAS #241 Splinter Session: Science from the TESS Extended Mission',
-             '',),
-     )
+    (
+        "<b>7th January 2023</b>",
+        "AAS #241 Workshop: NASAs TESS Mission Interactive Data Workshop",
+        "",
+    ),
+    (
+        "<b>10th January 2023</b>",
+        "AAS #241 Splinter Session: Science from the TESS Extended Mission",
+        "",
+    ),
+)
 
 # Defines the "meetings" box on the front page
 MEETINGS = (
-    ('<b>7th - 12th January 2023</b>',
-     'AAS #241',
-            'https://aas.org/meetings/aas241',),
-    ('<b>12th - 16th December 2022</b>',
-     'AGU Fall Metting',
-            'https://www.agu.org/Fall-Meeting',),
-            )
+    (
+        "<b>7th - 12th January 2023</b>",
+        "AAS #241",
+        "https://aas.org/meetings/aas241",
+    ),
+    (
+        "<b>12th - 16th December 2022</b>",
+        "AGU Fall Metting",
+        "https://www.agu.org/Fall-Meeting",
+    ),
+)
 
 # Defines the "related websites" listing in the footer of all pages
 RELATEDSITES = (
-            ('News, Media, and Education Resources',
-             'https://nasa.gov/tess'),
-            ('TESS @ MIT',
-             'http://tess.mit.edu/'),
-            # ('TESS @ Orbital ATK',
-            #  'http://www.ballaerospace.com/page.jsp?page=72'),
-            ('TESS @ MAST',
-             'http://archive.stsci.edu/tess'),
-            ('High Energy Astrophysics Science Archive Research Center',
-             'https://heasarc.gsfc.nasa.gov/'),
-            ('NASA Exoplanet Archive @ IPAC',
-             'http://exoplanetarchive.ipac.caltech.edu'),
-            )
+    ("News, Media, and Education Resources", "https://nasa.gov/tess"),
+    ("TESS @ MIT", "http://tess.mit.edu/"),
+    # ('TESS @ Orbital ATK',
+    #  'http://www.ballaerospace.com/page.jsp?page=72'),
+    ("TESS @ MAST", "http://archive.stsci.edu/tess"),
+    (
+        "High Energy Astrophysics Science Archive Research Center",
+        "https://heasarc.gsfc.nasa.gov/",
+    ),
+    ("NASA Exoplanet Archive @ IPAC", "http://exoplanetarchive.ipac.caltech.edu"),
+)
 
 SHOW_ARTICLE_AUTHOR = True
 DEFAULT_PAGINATION = 5
@@ -195,7 +214,7 @@ DISPLAY_CATEGORIES_ON_MENU = False
 BOOTSTRAP_NAVBAR_INVERSE = True
 
 
-DATE_MODIFIED = datetime.datetime.now().strftime('%Y-%m-%d')
+DATE_MODIFIED = datetime.datetime.now().strftime("%Y-%m-%d")
 
 PROPOSALBANNER = False
 PROPOSALMESSAGE = """The <a href="https://heasarc.gsfc.nasa.gov/docs/tess/proposing-investigations.html"><b>TESS Cycle 6 Call For Proposals</b></a> is now Live! To read the Call for Proposals please <a
